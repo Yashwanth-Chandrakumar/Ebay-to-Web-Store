bc1071227ac0825053594966eba325dae76990a2
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-08-06 16:02:23 +0530

    minor edit

--- backend/pjsautolit/products/urls.py
+++ backend/pjsautolit/products/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('update-product/<str:item_id>/', views.update_product_view, name='update_product'),
     path('generate-html/', views.generate_html_view, name='generate_html'),
     path('products/', views.get_products, name='get_products'),
+    path('fetch-items/', views.fetch_all_items, name='fetch_items'),
     path('product/<int:product_id>/', views.ProductDetailView.as_view(), name='product_detail'),
     
 ]
