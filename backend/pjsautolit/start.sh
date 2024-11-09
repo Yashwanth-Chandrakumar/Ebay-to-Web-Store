981dc8e3c1c02f1f8354fe78b54aa74e709c49fe
Yashwanth S C <yashwanth2k05@gmail.com>
2024-11-09 20:42:28 +0530

    Update start.sh

--- backend/pjsautolit/start.sh
+++ backend/pjsautolit/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Redis URL for Celery broker (update this with your actual Redis URL)
-export CELERY_BROKER_URL="rediss://red-cs9ruojqf0us739m39rg:Rn3HqACZpakhh0EL2dUdFWzWirA98TX4@oregon-redis.render.com:6379"
+export CELERY_BROKER_URL="redis://:pjsautolit@localhost:6379/0"
 
 # Start Celery worker in the background
 celery -A pjsautolit worker --pool=solo -l info &
