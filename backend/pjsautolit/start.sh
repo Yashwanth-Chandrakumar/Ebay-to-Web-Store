7e841b4fca5a21f60161cca4a32f2d4b04ba55f8
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-12-19 19:15:32 +0530

    Revert "Docker edit"
    This reverts commit 432f6d9f49abc5b8f1bdd66a156234a09262e886.

--- backend/pjsautolit/start.sh
+++ backend/pjsautolit/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Set Redis URL for Celery broker
+# Set Redis URL for Celery broker (update this with your actual Redis URL)
 export CELERY_BROKER_URL="redis://:pjsautolit@172.31.21.137:6379/0"
 
 # Start Celery worker in the background
@@ -16,19 +16,5 @@ forward_signal() {
 trap 'forward_signal TERM' TERM
 trap 'forward_signal INT'  INT
 
-# Start Gunicorn in the background
-gunicorn --bind 0.0.0.0:8000 --timeout 0 pjsautolit.wsgi:application &
-GUNICORN_PID=$!
-
-# Wait until Gunicorn is accepting connections
-echo "Waiting for Gunicorn to start on localhost:8000..."
-until curl -s http://localhost:8000 > /dev/null; do
-    sleep 1  # Wait 1 second before retrying
-done
-
-# Once Gunicorn is up, call the generate-html API
-echo "Calling generate-html API..."
-curl -X GET http://localhost:8000/generate-html/
-
-# Wait for all background processes
-wait $GUNICORN_PID
+# Start Gunicorn in the foreground
+exec gunicorn --bind 0.0.0.0:8000 --timeout 0 pjsautolit.wsgi:application
